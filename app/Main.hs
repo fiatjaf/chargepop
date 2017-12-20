2dd1ae934774062f1361134fb5fb4a4f56390c40
fiatjaf <fiatjaf@gmail.com>
2017-12-20 16:04:55 -0200

    spend and list spends.

--- app/Main.hs
+++ app/Main.hs
@@ -4,26 +4,29 @@
 module Main where
 
 import Control.Monad.IO.Class
-import Data.Text.Lazy (Text)
+import Data.Text.Lazy as T (Text, pack)
 import Web.Scotty
 import System.Environment
-import Data.ByteString.Char8 (pack)
+import Data.ByteString.Char8 as C8 (pack)
 import Database.PostgreSQL.Simple
 import Network.HTTP.Types.Status
+import Network.Wai.Middleware.RequestLogger
 import Data.Aeson (ToJSON, toJSON, object)
 import Data.Aeson.Types ((.=))
 import Data.Monoid ((<>))
 
 import Shop (Shop(..), getShop, parseToken)
-import User (User(..), getUser, ensureUser)
+import User (User(..), Spend(..), getUser, ensureUser, spend, getSpends)
 
 main :: IO ()
 main = do
   connString <- getEnv "DATABASE_URL"
-  db <- connectPostgreSQL $ pack connString
+  db <- connectPostgreSQL $ C8.pack connString
 
   putStrLn "Starting server..."
-  scotty 3000 $ server db
+  scotty 3000 $ do
+    middleware logStdoutDev
+    server db
 
 server :: Connection -> ScottyM()
 server db = do
@@ -52,9 +55,21 @@ server db = do
     created <- liftIO $ ensureUser db token u
     json $ Response True ("User successfully " <> if created then "created" else "updated") 
   get "/spend" $ do
-    text ""
+    token <- getToken
+    key <- param "user"
+    -- gte <- param "gte"
+    -- lte <- param "lte"
+    spends <- liftIO $ getSpends db token key
+    json spends
+    
   post "/spend" $ do
-    text ""
+    token <- getToken
+    key <- param "user"
+    amount <- param "amount"
+    desc <- param "description"
+    amt <- liftIO $ spend db token key (amount, desc)
+    json $ Response True (T.pack $ show amt <> " credits were spent")
+
 
 getToken :: ActionM Text
 getToken = fmap parseToken $ header "Authorization"
@@ -84,6 +99,15 @@ instance ToJSON UserResponse where
         ]
       , "status" .= ("success" :: Text)
       ]
+
+data SpendsResponse = SpendsResponse { spends :: [Spend] }
+instance ToJSON SpendsResponse where
+  toJSON (SpendsResponse spends) =
+    object
+      [ "spends" .= toJSON spends
+      , "status" .= ("success" :: Text)
+      ]
+
 
 data Response = Response { success :: Bool, message :: Text }
 instance ToJSON Response where
--- src/User.hs
+++ src/User.hs
@@ -2,21 +2,21 @@
 {-# LANGUAGE DeriveGeneric #-}
 module User
     ( User(..)
-    , getUser, ensureUser
+    , Spend(..)
+    , getUser, ensureUser, spend, getSpends
     ) where
 
 import GHC.Generics
 import Prelude hiding (id)
 import Data.Text.Lazy (Text)
-import Control.Monad
+import Data.Text.Lazy.Read
 import Data.Aeson (ToJSON, FromJSON, parseJSON, withObject, (.:), (.!=), (.:?))
 import Database.PostgreSQL.Simple
-import Database.PostgreSQL.Simple.ToRow
-import Database.PostgreSQL.Simple.ToField
+import Database.PostgreSQL.Simple.ToRow()
+import Database.PostgreSQL.Simple.ToField()
 import Database.PostgreSQL.Simple.FromRow
-import Database.PostgreSQL.Simple.FromField
+import Database.PostgreSQL.Simple.FromField()
 import Data.Function
-import Debug.Trace
 
 data User = User { key :: Text , email :: Text , balance :: Text }
   deriving (Show, Generic)
@@ -28,6 +28,12 @@ instance FromJSON User where
     <*> v .:? "balance" .!= ""
 instance FromRow User where
   fromRow = User <$> field <*> field <*> field
+
+data Spend = Spend { amount :: Double, description :: Text, spent_at :: Text }
+  deriving (Show, Generic)
+instance ToJSON Spend
+instance FromRow Spend where
+  fromRow = Spend <$> field <*> field <*> field
 
 getUser :: Connection -> Text -> Text -> IO [User]
 getUser conn token key =
@@ -45,3 +51,37 @@ ensureUser conn token (User key email _) =
 \ on conflict do nothing \
 \ " (key, email, token)
     & fmap (\c -> if c == 0 then False else True)
+
+spend :: Connection -> Text -> Text -> (Text, Text) -> IO Double
+spend conn token key (amount, desc) = do
+  execute conn "\
+\ with inserted as ( \
+\   insert into spends (shop, amount, description, user_key) \
+\   values ((select id from shops where token = ?), (? * 10000000), ?, ?) \
+\   returning * \
+\ ), \
+\ subtracted as ( \
+\   update users set balance = balance - (select amount from inserted) \
+\   where key = (select user_key from inserted) \
+\     and shop = (select shop from inserted) \
+\ ) \
+\ delete from spends \
+\ where shop = (select shop from inserted) \
+\   and timestamp < (select timestamp from inserted) - interval '3 months' \
+\ " (token, parseDouble amount, desc, key)
+  & fmap (\_ -> parseDouble amount)
+
+getSpends :: Connection -> Text -> Text -> IO [Spend]
+getSpends conn token key =
+  query conn "\
+\ select (amount::float / 10000000), description, timestamp::text from spends \
+\ inner join users on user_key = users.key \
+\ inner join shops on users.shop = shops.id \
+\ where key = ? and shops.token = ? \
+\ " (key, token)
+
+parseDouble :: Text -> Double
+parseDouble text =
+  case double text of
+    Left _ -> 0
+    Right (n, _) -> n
